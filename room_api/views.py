f20479255c6565fb81f179a4b77a0c8f54615819
gandastik <gandastik@gmail.com>
2021-11-13 20:31:26 +0700

    add api/room/id for PUT can decrease/increase room_free in RoomType

--- room_api/views.py
+++ room_api/views.py
@@ -35,6 +35,33 @@ class RoomDetailView(APIView):
        serializer = RoomSerializer(room)
        return Response(serializer.data)
 
+    def put(self, request, *args, **kwargs):
+        pk = self.kwargs['pk']
+        room = Room.objects.get(pk=pk)
+        serializer = RoomSerializer(room, data=request.data)
+        if(serializer.is_valid()):
+            roomtype = RoomType.objects.get(pk=room.id)
+            room_free = roomtype.room_free
+            if(room.isFree and request.data['isFree'] == False):
+                room_free = roomtype.room_free - 1
+            elif(room.isFree == False and request.data['isFree'] == True):
+                room_free = roomtype.room_free + 1
+            roomTypeSerializer = RoomTypeSerializer(roomtype, data={
+                'type': roomtype.type,
+                'max_price': roomtype.max_price,
+                'min_price': roomtype.min_price,
+                'type_name': roomtype.type_name,
+                'room_free': room_free,
+                'rating': roomtype.rating,
+                'pic': roomtype.pic
+            })
+            serializer.save()
+            if(roomTypeSerializer.is_valid()):
+                roomTypeSerializer.save()
+            print(roomtype.room_free)
+            return Response(serializer.data) 
+        return Response(serializer.errors, status=400)
+
     def delete(self, request, pk):
         room = generics.get_object_or_404(Room.objects.all(), pk=pk)
         room.delete()
